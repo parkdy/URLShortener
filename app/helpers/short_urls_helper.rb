b0f32794af9a2bf1d9dff459ad0984390b2ab288
David Park <parkdy6@gmail.com>
2013-11-11 23:30:49 -0500

    Change ShortUrls helper and view

--- app/helpers/short_urls_helper.rb
+++ app/helpers/short_urls_helper.rb
@@ -1,6 +1,11 @@
 module ShortUrlsHelper
-	# Create full url from ShortUrl object
+	# Create url from ShortUrl object without protocol
 	def url_string(short_url)
-		"#{request.protocol}#{request.host_with_port}/#{short_url.id}"
+		"#{request.host_with_port}/#{short_url.id}"
+	end
+
+	# Create full url from ShortUrl object with protocol
+	def full_url_string(short_url)
+		"#{request.protocol}#{url_string(short_url)}"
 	end
 end
